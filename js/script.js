331dfb7f6e24c8a1f53e810f9b81a92c8a330eb5
rrraaaa <zavarevicanik@gmailcom>
2023-12-27 22:37:00 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+burgerMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+console.log('');
+
+console.log('lkfjdkl');
+
